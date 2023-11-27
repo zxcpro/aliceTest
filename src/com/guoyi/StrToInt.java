482cbdd9e4e4ea6a24132673dc6a444a53c8964a
zxcpro <zhaoxuanchen77@gmail.com>
2023-11-27 18:18:53 +0800

    增加判空

--- src/com/guoyi/StrToInt.java
+++ src/com/guoyi/StrToInt.java
@@ -7,11 +7,14 @@ public class StrToInt {
      */
 
     public int strToInt(String s) {
-        if (s.isEmpty()) {
+        if (s == null || s.isEmpty()) {
             return 0;
         }
         //1、先把空格去掉
         char[] c = s.trim().toCharArray();
+        if(c == null || c.length == 0){
+            return 0;
+        }
         //2、res记录结果，sign记录符号，i记录数字起始位，从符号位后面，下标1开始
         int res = 0;
         int sign = 1;
@@ -27,7 +30,7 @@ public class StrToInt {
             //如果不允许有非数字，那就直接break，如果非数字过滤跳过，就continue
             if (c[j] < '0' || c[j] > '9') break;
             //4、判断字符的数字范围是否超过int，int的最后1位，正7付8
-            if (res > Integer.MAX_VALUE / 10 || res == Integer.MAX_VALUE / 10 && c[j] > '7') {
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && c[j] > '7')) {
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
             //5、res原res左移1位，加上当前值
@@ -38,7 +41,7 @@ public class StrToInt {
 
     public static void main(String[] args) {
         StrToInt strToInt = new StrToInt();
-        int num = strToInt.strToInt(" 67h8998");
+        int num = strToInt.strToInt(" ");
         System.out.println(num);
     }
 
