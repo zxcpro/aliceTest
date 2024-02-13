a3305becdbaf606d1f9f388da9f51918c3fd0d9b
zxc <zhaoxuanchen77@gmail.com>
2024-02-13 21:29:36 +0800

    21 合并两个有序链表

--- src/com/guoyi/MergeTwoLists.java
+++ src/com/guoyi/MergeTwoLists.java
@@ -1,30 +1,61 @@
 package com.guoyi;
+
 import com.guoyi.CommonTool.ListNode;
 
 public class MergeTwoLists {
-
-    //2、两数相加
     /*
-    给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
-    请你将两个数相加，并以相同形式返回一个表示和的链表。
-    你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
-    输入：l1 = [2,4,3], l2 = [5,6,4]
-    输出：[7,0,8]
-    解释：342 + 465 = 807
+    21、合并两个有序链表
+    将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+    示例 1：
+    输入：l1 = [1,2,4], l2 = [1,3,4]
+    输出：[1,1,2,3,4,4]
+    示例 2：
+    输入：l1 = [], l2 = []
+    输出：[]
+    示例 3：
+    输入：l1 = [], l2 = [0]
+    输出：[0]
      */
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
-        } else if (l1.val < l2.val) {
-            l1.next = mergeTwoLists(l1.next, l2);
-            return l1;
-        } else {
-            l2.next = mergeTwoLists(l1, l2.next);
-            return l2;
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummyHead = new ListNode();
+        ListNode current = dummyHead;
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+
+        while (true) {
+            ListNode target;
+            if (temp1 == null && temp2 == null) {
+                break;
+            }
+            if (temp1 != null && temp2 == null) {
+                target = temp1;
+                temp1 = temp1.next;
+            } else if (temp2 != null && temp1 == null) {
+                target = temp2;
+                temp2 = temp2.next;
+            } else if (temp1.val < temp2.val) {
+                target = temp1;
+                temp1 = temp1.next;
+            } else {
+                target = temp2;
+                temp2 = temp2.next;
+            }
+            current.next = target;
+            current = target;
         }
+
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(3);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(7);
+        ListNode l2 = new ListNode(5, l1);
+        MergeTwoLists mergeTwoLists = new MergeTwoLists();
+        ListNode listNode1 = mergeTwoLists.mergeTwoLists(l1, l2);
+        System.out.println(listNode1.toString());
     }
 
 }
