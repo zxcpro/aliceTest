9a2166c378c7a67f57ba453e538ac80222fac4c9
zxc <zhaoxuanchen77@gmail.com>
2024-02-24 20:42:16 +0800

    86 分割链表

--- src/com/guoyi/ReverseList.java
+++ src/com/guoyi/ReverseList.java
@@ -12,10 +12,20 @@ public class ReverseList {
     输出：[5,4,3,2,1]
      */
 
+    /*
+    思路：双指针，反转方向
+    1、初始化当前指针cur、前一个指针pre
+    2、cur向后遍历，到null为止
+    3、初始化一个temp记录cur.next的位置。
+    cur指向pre（重要，，转向了）
+    pre、cur都向后移动一位
+    4、返回：pre
+     */
     public ListNode reverseList(ListNode head) {
         ListNode cur = head;
         ListNode pre = null;
         while (cur != null) {
+            //初始化一个
             ListNode temp = cur.next;
             cur.next = pre;
             pre = cur;
