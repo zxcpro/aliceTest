53a0a510fad9426c1846cfd802fc3c696ef57c9b
zxcpro <zhaoxuanchen77@gmail.com>
2024-04-08 10:34:01 +0800

    206 反转链表

---
+++ src/com/guoyi/ReverseList.java
@@ -0,0 +1,19 @@
+package com.guoyi;
+
+import com.guoyi.CommonTool.ListNode;
+
+public class ReverseList {
+
+    /*
+    206 反转链表
+     */
+
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+
+        }
+        return null;
+    }
+}
